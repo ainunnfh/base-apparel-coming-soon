e09335b79c17d34951e6c4ed948bfc9527d496db
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-01-25 22:35:22 +0700

    💄update (page.tsx): create email address design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
         <img className="w-32 ml-5" src="/logo.svg"></img>
         <img className="w-full  " src="/hero-mobile.jpg"></img>
       </div>
-      <div className="text-[#d8acac] p-6 flex flex-col h-1/2 justify-around items-center text-center">
-        <div className="flex flex-col gap-4">
-          <p className="text-5xl font-josefinSans uppercase font-light">
+      <div className="text-[#d8acac] p-6 flex flex-col items-center text-center">
+        <div className="flex flex-col gap-4 h-[50vh] justify-around">
+          <p className="text-5xl font-josefinSans uppercase">
             We&apos;re <br />
-            <span className="text-[#413a3a]">
+            <span className="text-[#413a3a] ">
               Coming
               <br /> Soon
             </span>
@@ -21,9 +21,19 @@ export default function Home() {
             store. Add your email below to stay up-to-date with announcements
             and our launch deals.
           </p>
-          <p className="border rounded-full px-4 py-3 border-[#d8acac] ">
-            email addresess
-          </p>
+
+          <div className="border flex  rounded-full px-6 py-6 border-[#d8acac] relative">
+            <p className="absolute text-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              Email Addresess
+            </p>
+            <p className="absolute top-0 right-0">
+              <img
+                className=" bg-gradient-to-r from-[#f8bfbf]  to-[#ee8c8c] shadow-xl rounded-full px-7 py-4 shadow-pink-200"
+                src="/icon-arrow.svg"
+                alt="arrow icon"
+              />
+            </p>
+          </div>
         </div>
       </div>
     </div>
