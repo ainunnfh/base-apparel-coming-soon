0f7944ebaec720b3c6ceb151fa56c462bc0151a0
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-01-26 20:48:46 +0700

    💄update (page.tsx): create desktop size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,42 @@
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen ">
+    <div className="flex flex-col h-screen md:flex-row-reverse">
       <div className="flex flex-col gap-4">
-        <img className="w-32 ml-5 pt-5" src="/logo.svg"></img>
-        <img className="w-full object-cover" src="/hero-mobile.jpg"></img>
+        {/* mobile size */}
+        <img className="w-32 ml-5 pt-5  md:hidden" src="/logo.svg"></img>
+        <img
+          className="w-full  object-cover md:hidden"
+          src="/hero-mobile.jpg"
+        ></img>
+
+        {/* desktop size */}
+
+        <img
+          className="w-full h-screen bg-green-900 object-cover hidden md:block"
+          src="/hero-desktop.jpg"
+        ></img>
       </div>
-      <div className="text-[#d8acac] p-6 flex flex-col items-center text-center">
-        <div className="flex flex-col gap-4 h-[50vh] justify-around">
-          <p className="text-5xl font-josefinSans uppercase">
+      <div className="text-[#d8acac] p-6 flex flex-col items-center text-center md:items-start md:justify-start md:text-start ">
+        <div className="flex flex-col h-[50vh] justify-around md:flex-grow">
+          <img
+            className="w-32 ml-5 pt-5 hidden md:block md:ml-10"
+            src="/logo.svg"
+          ></img>
+          <p className="text-5xl font-josefinSans uppercase md:text-6xl md:p-10">
             We&apos;re <br />
             <span className="text-[#413a3a] ">
               Coming
               <br /> Soon
             </span>
           </p>
-          <p className="text-[#d8acac] ">
+          <p className="text-[#d8acac]  md:p-10">
             Hello Fellow shoppers! We&apos;re currently building our new fashion
             store. Add your email below to stay up-to-date with announcements
             and our launch deals.
           </p>
 
-          <div className="border flex  rounded-full px-6 py-6 border-[#d8acac] relative text-[#d8acac]">
+          <div className="border flex  rounded-full px-6 py-6 md:ml-10 border-[#d8acac] relative text-[#d8acac] ">
             <form className="absolute text-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
               <input
                 type="email"
